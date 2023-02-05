ff029634dcc68c91d6b90133fcfa35beeee2c77c
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-05 12:02:20 +0530

    Init

---
+++ src/test/java/com/sameera/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sameera.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
