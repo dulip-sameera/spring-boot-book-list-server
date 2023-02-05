ae0c47ee990331bebde6df36dec61751eec63716
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-05 12:22:55 +0530

    create repository interface

---
+++ src/main/java/com/sameera/server/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.sameera.server.repository;
+
+import com.sameera.server.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
